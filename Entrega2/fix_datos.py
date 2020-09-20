32200178f7e5ad5cd4a5e5e706b1741f6ea21733
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-09-20 13:00:19 -0300

    Entrega 2 empezada

---
+++ Entrega2/fix_datos.py
@@ -0,0 +1,17 @@
+import os
+
+with open("Datos/personal_instalacion.csv", "r") as archivo:
+	info = archivo.readlines()
+	new_info = []
+	for line in info:
+		line = line.strip().split(",")
+		new_line = [line[1], line[0], line[2], line[3], line[4]]
+		new_info.append(new_line)
+
+with open("Datos/new_persona.csv", "w") as new_archivo:
+	new_lines = []
+	for line in new_info:
+		new_string = line[0] + "," + line[1] + "," + line[2] + "," + line[3] + "," + line[4] + "\n"
+		new_lines.append(new_string)
+	new_archivo.writelines(new_lines)
+	
