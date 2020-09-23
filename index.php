4df21cfec84df63c777d65ce4ee626e0730d5fd2
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-09-23 19:02:11 -0300

    php agregado

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+  $user = 'grupo53';
+  $password = 'huertitaismybae';
+  $databaseName = 'grupo53';
+  $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+
+  $query_string = "SELECT * FROM tabla;";
+  $query = $db -> prepare($query_string);
+  $query -> execute();
+  $result = $query -> fetchAll();
+?>
+
+<table>
+  <tr>
+    <th>ID</th>
+    <th>Nombre</th>
+    <th>Altura</th>
+  </tr>
+  
+  <?php
+    foreach ($result as $r) {
+      echo "<tr><td>$r[0]</td><</tr>";
+    }
+  ?>
+</table>
