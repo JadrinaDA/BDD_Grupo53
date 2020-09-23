d6a99d687f3d8acb33bcafa5bc73c96dec5480ce
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-09-23 20:16:07 -0300

    added html and css

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<!DOCTYPE html>
+<html lang = "en">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name ="viewport" content = "width= device-width, initial-scale= 1.0" />
+  <title> Grupo 53: Entrega 2</title>
+  <link rel="icon" type="image/png" href="Icono.png">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link rel = "stylesheet" href = "main.css">
+  <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Yanone+Kaffeesatz&display=swap" rel="stylesheet">
+</head>
+
 <?php
   $user = 'grupo53';
   $password = 'huertitaismybae';
@@ -29,3 +42,5 @@
     }
   ?>
 </table>
+
+</html>
