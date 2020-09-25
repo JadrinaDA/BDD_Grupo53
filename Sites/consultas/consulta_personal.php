78d7f3ccb4d835714ac0014ec4a78325435cc903
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-09-25 16:25:00 -0300

    button buscar

---
+++ Sites/consultas/consulta_personal.php
@@ -0,0 +1,28 @@
+<body>
+  
+<?php
+  require("config/conexion.php");
+  $query_string = "SELECT * FROM Personal;";
+  $query = $db -> prepare($query_string);
+  $query -> execute();
+  $result = $query -> fetchAll();
+?>
+
+<table>
+  <tr>
+    <th>RUT</th>
+    <th>Nombre</th>
+    <th>Edad</th>
+    <th>Sexo</th>
+  </tr>
+  
+  <?php
+    foreach ($result as $r) {
+      echo "<tr>";
+      echo "<td>".$r[0]."</td>";
+      echo "<td>".$r[1]."</td>";
+      echo "<td>".$r[2]."</td>";
+      echo "<td>".$r[3]."</td>";
+      echo "</tr>";
+    }
+  ?>
--- Sites/index.php
+++ Sites/index.php
@@ -29,34 +29,13 @@
   <br>
   <br>
   <br>
+
+  <h3 align="center">¿Quieres ver todos los puertos y sus ciudades?</h3>
+
+  <button type="button" class="btn btn-primary">Buscar</button>
+
 </body>
 
-<?php
-  require("config/conexion.php");
-  $query_string = "SELECT * FROM Personal;";
-  $query = $db -> prepare($query_string);
-  $query -> execute();
-  $result = $query -> fetchAll();
-?>
-
-<table>
-  <tr>
-    <th>RUT</th>
-    <th>Nombre</th>
-    <th>Edad</th>
-    <th>Sexo</th>
-  </tr>
-  
-  <?php
-    foreach ($result as $r) {
-      echo "<tr>";
-      echo "<td>".$r[0]."</td>";
-      echo "<td>".$r[1]."</td>";
-      echo "<td>".$r[2]."</td>";
-      echo "<td>".$r[3]."</td>";
-      echo "</tr>";
-    }
-  ?>
 </table>
 
 </html>
