f7eaf6fdc9db3b68f16699227704fd48370b47c2
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-09-26 10:08:39 -0300

    Fixed typo C5

--- Sites/consultas/consulta_personal_edad.php
+++ Sites/consultas/consulta_personal_edad.php
@@ -5,7 +5,7 @@
 <?php
   require("../config/conexion.php");
 
-  $query_string = "SELECT puertos.nombre,AVG(personal.edad) as Edad_Promedio_Por_Puerto FROM personal,trabaja_en,pertenece_a,puertsonal.rut=trabaja_en.rut AND trabaja_en.iid=pertenece_a.iid AND pertenece_a.nombre_puerto=puertos.nombre GROUP BY puertos.nombre;";
+  $query_string = "SELECT puertos.nombre,AVG(personal.edad) as Edad_Promedio_Por_Puerto FROM personal,trabaja_en,pertenece_a, puertos WHERE personal.rut=trabaja_en.rut AND trabaja_en.iid=pertenece_a.iid AND pertenece_a.nombre_puerto=puertos.nombre GROUP BY puertos.nombre;";
   $query = $db -> prepare($query_string);
   $query -> execute();
   $result = $query -> fetchAll();
