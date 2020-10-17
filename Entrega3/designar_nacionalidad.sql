d0a3670f557573b10b59fb2ea284d4fb631e456e
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-17 19:24:36 -0300

    separating attributes

--- Entrega3/designar_nacionalidad.sql
+++ Entrega3/designar_nacionalidad.sql
@@ -5,7 +5,7 @@ DECLARE
 BEGIN
 CREATE TABLE tabla_aux(nacionalidad VARCHAR);
 INSERT INTO tabla_aux VALUES('CHILENA');
-RETURN QUERY EXECUTE 'SELECT * FROM (SELECT personal.rut,nombre,edad,sexo,iid FROM personal,jefes WHERE personal.rut=jefes.rut) AS consulta CROSS JOIN tabla_aux';
+RETURN QUERY EXECUTE 'SELECT rut,nombre, edad, sexo, iid, nacionalidad FROM (SELECT personal.rut,nombre,edad,sexo,iid FROM personal,jefes WHERE personal.rut=jefes.rut) AS consulta CROSS JOIN tabla_aux';
 DROP TABLE tabla_aux;
 END;
 $$ language plpgsql 
