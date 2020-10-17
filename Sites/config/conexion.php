44c9da908907d3e7dcb68b76fab8498d5a692aa7
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-17 16:53:07 -0300

    fixed syntax error

--- Sites/config/conexion.php
+++ Sites/config/conexion.php
@@ -4,7 +4,7 @@
   $password_buques = 'danatas13';
   $password_puertos = 'huertitaismybae';
   $databaseName_puertos = 'grupo53e3';
-  $databaseName_buques = 'grupo14e3'
+  $databaseName_buques = 'grupo14e3';
   $db_puertos = new PDO("pgsql:dbname=$databaseName_puertos;host=localhost;port=5432;user=$user_puertos;password=$password_puertos");
  $db_buques = new PDO("pgsql:dbname=$databaseName_buques;host=localhost;port=5432;user=$user_buques;password=$password_buques");
   ?>
