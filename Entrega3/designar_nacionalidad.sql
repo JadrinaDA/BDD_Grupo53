1e0dcf03fefda225a9b909479fe81aedda3babea
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-17 17:33:39 -0300

    added nacionalidad

--- Entrega3/designar_nacionalidad.sql
+++ Entrega3/designar_nacionalidad.sql
@@ -1,10 +1,13 @@
 # LO QUE ME RETORNA LA CONSULTA SELECT * FROM personal,jefes 
 # WHERE personal.rut=jefes.rut; (rut,nombre,edad,sexo,rut,iid)
 
-SELECT * FROM personal,jefes WHERE personal.rut=jefes.rut;
+
 CREATE OR REPLACE FUNCTION nacionalizar()
 RETURNS TABLE (rut VARCHAR, nombre VARCHAR, edad INT, sexo VARCHAR, iid INT, nacionalidad VARCHAR) 
 AS $$
 DECLARE
 BEGIN
-END
+CREATE TABLE tabla_aux(nacionalidad VARCHAR);
+RETURN SELECT * FROM (SELECT * FROM personal,jefes WHERE personal.rut=jefes.rut) CROSS JOIN tabla_aux;
+DROP TABLE tabla_aux;
+END;
