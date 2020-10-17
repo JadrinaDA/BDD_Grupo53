45f8ba56b4108d40d7b70eb933d7949ffc445f84
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-17 17:22:27 -0300

    archivos procesos almacenados

---
+++ Entrega3/designar_nacionalidad.sql
@@ -0,0 +1,5 @@
+# LO QUE ME RETORNA LA CONSULTA SELECT * FROM personal,jefes 
+# WHERE personal.rut=jefes.rut; (rut,nombre,edad,sexo,rut,iid)
+
+SELECT * FROM personal,jefes WHERE personal.rut=jefes.rut;
+
