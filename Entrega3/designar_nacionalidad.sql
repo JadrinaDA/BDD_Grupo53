2cf097e4acdfe60f11da76e21712ac319c234a40
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-17 17:25:48 -0300

    archivos procesos almacenados con los datos jeje

--- Entrega3/designar_nacionalidad.sql
+++ Entrega3/designar_nacionalidad.sql
@@ -2,4 +2,9 @@
 # WHERE personal.rut=jefes.rut; (rut,nombre,edad,sexo,rut,iid)
 
 SELECT * FROM personal,jefes WHERE personal.rut=jefes.rut;
-
+CREATE OR REPLACE FUNCTION nacionalizar()
+RETURNS TABLE (rut VARCHAR, nombre VARCHAR, edad INT, sexo VARCHAR, iid INT, nacionalidad VARCHAR) 
+AS $$
+DECLARE
+BEGIN
+END
