aa03bc3e660c3baf642dfa33285ab0df60b9525d
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-18 13:34:31 -0300

    testing select

--- Entrega3/verificacion.sql
+++ Entrega3/verificacion.sql
@@ -1,11 +1,10 @@
-##(nombre,edad,sexo,contraseña,nacionalidad,pasaporte_rut)
 CREATE OR REPLACE FUNCTION verificar_si_existe(pasaporte_rut VARCHAR)
 RETURNS bool 
 AS $$
 DECLARE
-tupla RECORD;
+tupla INTEGER;
 BEGIN;
-tupla := EXECUTE QUERY 'SELECT COUNT(*) FROM Usuarios WHERE Usuarios.pasaporte = pasaporte_rut' USING pasaporte_rut;
+tupla := (SELECT COUNT(*) FROM Personal WHERE Personal.pasaporte = pasaporte_rut USING pasaporte_rut);
 IF tupla != 0 THEN RETURN TRUE;
 ELSE RETURN FALSE; END IF;
 END;
