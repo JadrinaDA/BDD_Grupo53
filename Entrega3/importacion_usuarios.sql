1394e211fec146cc895180290a002ad740a90809
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-19 16:36:31 -0300

    crear variable nacionalidad

--- Entrega3/importacion_usuarios.sql
+++ Entrega3/importacion_usuarios.sql
@@ -5,11 +5,13 @@ tupla_53 RECORD;
 tupla_14 RECORD;
 contador INTEGER;
 sexo VARCHAR;
+nacionalidad VARCHAR;
 BEGIN
 contador := 0;
+nacionalidad := "CHILENA";
 FOR tupla_53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
 LOOP
-INSERT INTO usuarios VALUES(contador,tupla_53.nombre,tupla_53.edad,tupla_53.sexo,tupla_53.rut,"CHILENA",aleatorio());
+INSERT INTO usuarios VALUES(contador,tupla_53.nombre,tupla_53.edad,tupla_53.sexo,tupla_53.rut,nacionalidad,aleatorio());
 contador := contador + 1;
 END LOOP;
 END;
