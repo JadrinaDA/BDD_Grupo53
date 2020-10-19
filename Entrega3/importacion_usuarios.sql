91d8c85e1424ae9a51c1ee3c9199a5de534c8fe7
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-19 16:14:55 -0300

    cambiar nombre tupla

--- Entrega3/importacion_usuarios.sql
+++ Entrega3/importacion_usuarios.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE FUNCTION imoprtar_usuarios()
 RETURNS VOID AS $$
 DECLARE
-tupla_53 RECORD;
-tupla_14 RECORD;
+tupla53 RECORD;
+tupla14 RECORD;
 contador INTEGER;
 BEGIN
 contador := 0
-FOR tupla_53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
+FOR tupla53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
 LOOP
-INSERT INTO intento_1 VALUES(contador,tupla_53.rut);
+INSERT INTO intento_1 VALUES(contador,tupla53.rut);
 contador := contador + 1;
 END LOOP;
 END;
