ce9af085906d31c555e34f61c1cbc26a99a78abf
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-19 16:32:58 -0300

    intento de como quedaria USUARIOS solo usando GRUPO53

--- Entrega3/importacion_usuarios.sql
+++ Entrega3/importacion_usuarios.sql
@@ -4,11 +4,12 @@ DECLARE
 tupla_53 RECORD;
 tupla_14 RECORD;
 contador INTEGER;
+sexo VARCHAR;
 BEGIN
 contador := 0;
 FOR tupla_53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
 LOOP
-INSERT INTO intento_1 VALUES(contador,tupla_53.rut);
+INSERT INTO usuarios VALUES(contador,tupla_53.nombre,tupla_53.edad,tupla_53.sexo,tupla_53.rut,"CHILENA",aleatorio());
 contador := contador + 1;
 END LOOP;
 END;
