e6a129b775d856a6e2934a25ca20017bcdcb1efa
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-19 19:41:45 -0300

    intento 1 de migracion de datos

--- Entrega3/importacion_usuario_buques.sql
+++ Entrega3/importacion_usuario_buques.sql
@@ -1,28 +1,21 @@
-CREATE OR REPLACE FUNCTION imoprtar_usuarios()
+CREATE OR REPLACE FUNCTION importar_usuarios_buques()
 RETURNS VOID AS $$
 DECLARE
-tupla_53 RECORD;
 tupla_14 RECORD;
 contador INTEGER;
 sexo VARCHAR;
-nacionalidad VARCHAR;
 clave VARCHAR;
 BEGIN
-clave := 'apple369'
-contador := 0;
-nacionalidad := 'CHILENA';
-FOR tupla_53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
-LOOP
-INSERT INTO usuarios VALUES(contador,tupla_53.nombre,tupla_53.edad,tupla_53.sexo,tupla_53.rut,nacionalidad,clave);
+clave := 'apple369';
+contador := (SELECT COUNT(Usuarios.uid) FROM Usuarios);
 contador := contador + 1;
-END LOOP;
 FOR tupla_14 IN SELECT * FROM personal WHERE es_capitan = 't'
 LOOP
 sexo := '';
 IF tupla_14.genero = 'mujer' THEN sexo := 'F';
 ELSE sexo := 'M'; END IF;
 INSERT INTO usuarios VALUES(contador,tupla_14.pnombre,tupla_14.edad,sexo,tupla_14.pasaporte,tupla_14.nacionalidad,clave);
-contador := contador +1;
+contador := contador + 1;
 END LOOP;
 END;
 $$ language plpgsql 
---
+++ Entrega3/importacion_usuarios_puertos.sql
@@ -0,0 +1,22 @@
+CREATE OR REPLACE FUNCTION importar_usuarios_puertos()
+RETURNS TABLE (contador INTEGER, nombre VARCHAR, edad INTEGER, sexo VARCHAR, pasaporte VARCHAR, nacionalidad VARCHAR, clave VARCHAR)
+AS $$
+DECLARE
+tupla_53 RECORD;
+contador INTEGER;
+nacionalidad VARCHAR;
+clave VARCHAR;
+BEGIN
+CREATE TABLE tabla_auxiliar(contador INTEGER, nombre VARCHAR, edad INTEGER, sexo VARCHAR, pasaporte VARCHAR, nacionalidad VARCHAR, clave VARCHAR);
+clave := 'apple369';
+contador := 0;
+nacionalidad := 'CHILENA';
+FOR tupla_53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
+LOOP
+INSERT INTO tabla_auxiliar VALUES(contador,tupla_53.nombre,tupla_53.edad,tupla_53.sexo,tupla_53.rut,nacionalidad,clave);
+contador := contador + 1;
+END LOOP;
+RETURN tabla_auxiliar;
+DROP TABLE tabla_auxiliar;
+END;
+$$ language plpgsql 
