8c8d6e339d87b5dfbc9a89145591a0d4f1fe8f05
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-19 16:13:38 -0300

    intento contador

--- Entrega3/importacion_usuarios.sql
+++ Entrega3/importacion_usuarios.sql
@@ -1,11 +1,15 @@
 CREATE OR REPLACE FUNCTION imoprtar_usuarios()
 RETURNS VOID AS $$
 DECLARE
-tupla RECORD;
+tupla_53 RECORD;
+tupla_14 RECORD;
+contador INTEGER;
 BEGIN
-FOR tupla IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
+contador := 0
+FOR tupla_53 IN SELECT * FROM personal,jefes WHERE personal.rut = jefes.rut
 LOOP
-INSERT INTO intento_1 VALUES(tupla.nombre,tupla.rut);
+INSERT INTO intento_1 VALUES(contador,tupla_53.rut);
+contador := contador + 1;
 END LOOP;
 END;
 $$ language plpgsql 
