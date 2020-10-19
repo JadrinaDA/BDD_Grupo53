a7c356d6fe9a97148693659050a56f0c336cb96a
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-19 16:50:41 -0300

    la migracion funciona para nuestra base de datos! GRUPO53! OJO que el archivo que estoy subiendo tiene el codigo completo, asique ahora en nuestra base de datos NO FUNCIONARA

--- Entrega3/importacion_usuarios.sql
+++ Entrega3/importacion_usuarios.sql
@@ -14,5 +14,13 @@ LOOP
 INSERT INTO usuarios VALUES(contador,tupla_53.nombre,tupla_53.edad,tupla_53.sexo,tupla_53.rut,nacionalidad,aleatorio());
 contador := contador + 1;
 END LOOP;
+FOR tupla_14 IN SELECT * FROM personal WHERE es_capitan = 't'
+LOOP
+sexo := '';
+IF tupla_14.genero = 'mujer' THEN sexo := 'F';
+ELSE sexo := 'M'; END IF;
+INSERT INTO usuarios VALUES(contador,tupla_14.pnombre,tupla_14.edad,sexo,tupla_14.pasaporte,tupla_14.nacionalidad,aleatorio());
+contador := contador +1;
+END LOOP;
 END;
 $$ language plpgsql 
