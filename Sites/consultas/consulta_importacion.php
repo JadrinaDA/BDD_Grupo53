27decd7b440e26a2c877732e2543ed1a15edbe49
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-20 12:30:44 -0300

    put prepare in loop

--- Sites/consultas/consulta_importacion.php
+++ Sites/consultas/consulta_importacion.php
@@ -15,8 +15,8 @@
   $result_puerto = $query_puertos -> fetchAll();
   $query_string_insert = "INSERT INTO usuarios VALUES ('$user[0]', '$user[1]',
   '$user[2]', '$user[3]', '$user[4]', '$user[5]', '$user[6]');";
-  $query_insert = $db_buques -> prepare($query_string_insert);
   foreach ($result_puerto as $user){
+    $query_insert = $db_buques -> prepare($query_string_insert);
     $query_insert -> execute();
   }
   $query_string_buques = "SELECT importar_usuarios_buques();";
