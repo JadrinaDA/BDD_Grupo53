05ec743178cc189cd69ffcbd3931977380d05255
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-21 18:32:49 -0300

    using a table instead

--- Entrega3/calcular_capacidad.sql
+++ Entrega3/calcular_capacidad.sql
@@ -8,28 +8,30 @@ capacidad_max INT;
 atracados INT;
 BEGIN
 CREATE TABLE table_cap(iid INT, tiene_capacidad bool);
-atracados := 0;
-FOR tupla_muelle IN SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT atraques.pid, instalaciones.iid, instalaciones.capacidad FROM instalaciones
+SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque INTO table_moors FROM ((SELECT atraques.pid, instalaciones.iid, instalaciones.capacidad FROM instalaciones
  INNER JOIN atraques ON instalaciones.iid = atraques.iid WHERE instalaciones.tipo = 'muelle') AS insts INNER JOIN permisos ON insts.pid = permisos.pid)
+atracados := 0;
+FOR tupla_muelle IN instalaciones
 LOOP
-IF tupla_muelle.fecha_atraque >= fecha_start AND tupla_muelle.fecha_atraque <= fecha_end
+IF tupla_muelle.fecha_atraque = fecha_start
 THEN 
-atracados := atracados + 1;
+atracados := (SELECT COUNT(*) FROM table_moors WHERE table_moors.iid == tupla_muelle.iid);
 END IF;
 capacidad_max := tupla_muelle.capacidad;
 INSERT INTO table_cap VALUES(tupla_muelle.iid, atracados < capacidad_max);
 END LOOP;
-atracados := 0;
+DROP TABLE table_moors;
+/*atracados := 0;
 FOR tupla_asti IN SELECT permisos_astillero.pid, astis.iid, astis.capacidad, astis.fecha_atraque, permisos_astillero.fecha_salida FROM ((SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT pid, instalaciones.iid, instalaciones.capacidad FROM instalaciones
  INNER JOIN atraques ON instalaciones.iid = atraques.iid WHERE instalaciones.tipo = 'astillero') AS insts INNER JOIN permisos ON insts.pid = permisos.pid)) as astis INNER JOIN permisos_astillero ON astis.pid = permisos_astillero.pid)
 LOOP
-IF tupla_asti.fecha_atraque >= fecha_start AND tupla_asti.fecha_atraque <= fecha_end
+IF tupla_asti.fecha_atraque >= fecha_start AND tupla_asti.fecha_atraque <= fecha_end AND tupla_asti.fecha_salida >
 THEN 
 atracados := atracados + 1;
 END IF;
 capacidad_max := tupla_asti.capacidad;
 INSERT INTO table_cap VALUES(tupla_asti.iid, atracados < capacidad_max);
-END LOOP;
+END LOOP;*/
 RETURN QUERY EXECUTE 'SELECT * FROM table_cap';
 DROP TABLE table_cap;
 END;
