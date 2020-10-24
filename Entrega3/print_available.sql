ab81c6e53e8095de6645f7d7b4a7d5cb891db8cd
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-24 14:51:16 -0300

    string processing changed

--- Entrega3/print_available.sql
+++ Entrega3/print_available.sql
@@ -3,11 +3,15 @@ RETURNS TABLE(iid INT, tipo VARCHAR(100), capacidad INT)
 AS $$
 DECLARE
 tupla RECORD;
+pos_com INT;
+string CHAR(100);
 BEGIN
 CREATE TABLE tabla_aux(iid INT, tiene_capacidad bool);
 FOR tupla IN SELECT calcular_capacidad(fecha_start, fecha_end)
 LOOP
-INSERT INTO tabla_aux VALUES(CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR(100)) , 2, 1) AS INT), CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR(100)) , 4, 1) AS bool));
+string := CAST(tupla.calcular_capacidad AS CHAR(100);
+pos_com := POSITION(',' IN string)
+INSERT INTO tabla_aux VALUES(CAST(SUBSTRING(string) , 2, pos_com - 2) AS INT), CAST(SUBSTRING(CAST(string) , pos_com, 1) AS bool));
 END LOOP;
 RETURN QUERY EXECUTE 'SELECT * FROM instalaciones INNER JOIN tabla_aux ON instalaciones.iid == tabla_aux.iid WHERE instalaciones.tipo = tipo_chosen';
 DROP TABLE tabla_aux;
