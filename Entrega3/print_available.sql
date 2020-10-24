38749ebb686a4fbfc448cddb6be6be8d945a191a
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-24 14:45:22 -0300

    added length

--- Entrega3/print_available.sql
+++ Entrega3/print_available.sql
@@ -1,15 +1,15 @@
 CREATE OR REPLACE FUNCTION print_available(tipo_chose VARCHAR, fecha_start DATE, fecha_end DATE)
-RETURNS TABLE(textier CHAR)--iid INT, tipo VARCHAR(100), capacidad INT) 
+RETURNS TABLE(iid INT, tipo VARCHAR(100), capacidad INT) 
 AS $$
 DECLARE
 tupla RECORD;
 BEGIN
-CREATE TABLE tabla_aux(texty CHAR);--iid INT, tiene_capacidad bool);
+CREATE TABLE tabla_aux(iid INT, tiene_capacidad bool);
 FOR tupla IN SELECT calcular_capacidad(fecha_start, fecha_end)
 LOOP
-INSERT INTO tabla_aux VALUES(tupla);--(CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR) , 2, 1) AS INT), CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR) , 4, 1) AS bool));
+INSERT INTO tabla_aux VALUES(CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR(100)) , 2, 1) AS INT), CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR(100)) , 4, 1) AS bool));
 END LOOP;
-RETURN QUERY EXECUTE 'SELECT * FROM tabla_aux';--FROM instalaciones INNER JOIN tabla_aux ON instalaciones.iid == tabla_aux.iid WHERE instalaciones.tipo = tipo_chosen';
+RETURN QUERY EXECUTE 'SELECT * FROM instalaciones INNER JOIN tabla_aux ON instalaciones.iid == tabla_aux.iid WHERE instalaciones.tipo = tipo_chosen';
 DROP TABLE tabla_aux;
 END;
 $$ language plpgsql 
