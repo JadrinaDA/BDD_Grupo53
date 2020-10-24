86870eb2ed7fd7e239ea1f05f42c90ad9a539cf3
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-24 14:32:30 -0300

    type casted again

--- Entrega3/print_available.sql
+++ Entrega3/print_available.sql
@@ -7,7 +7,7 @@ BEGIN
 CREATE TABLE tabla_aux(iid INT, tiene_capacidad bool);
 FOR tupla IN SELECT calcular_capacidad(fecha_start, fecha_end)
 LOOP
-INSERT INTO tabla_aux VALUES(CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR) , 2, 1) AS INT), SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR) , 4, 1));
+INSERT INTO tabla_aux VALUES(CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR) , 2, 1) AS INT), CAST(SUBSTRING(CAST(tupla.calcular_capacidad AS CHAR) , 4, 1) AS bool));
 END LOOP;
 RETURN QUERY EXECUTE 'SELECT * FROM instalaciones INNER JOIN tabla_aux ON instalaciones.iid == tabla_aux.iid WHERE instalaciones.tipo = tipo_chosen';
 DROP TABLE tabla_aux;
