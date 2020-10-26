2516218419e1d8cf69e4fe6307f23aa625c8b2fa
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-26 15:36:08 -0300

    pls work

--- Entrega3/todos_dias_disponibles.sql
+++ Entrega3/todos_dias_disponibles.sql
@@ -145,7 +145,8 @@ END LOOP;
 FOR tupla_dias_permiso_auxiliar IN SELECT * FROM (SELECT puertos.nombre,pertenece_a.iid FROM puertos, pertenece_a WHERE puertos.nombre=pertenece_a.nombre_puerto) AS consulta_aux WHERE nombre=puerto
 LOOP
 id := tupla_dias_permiso_auxiliar.iid;
-INSERT INTO tabla_dias_disponibles_cesgados VALUES(CONCAT(id),CONCAT(id));
+SELECT INTO tupla_auxiliar * FROM tabla_dias_disponibles WHERE tabla_dias_disponibles.instalaciones_id=id;
+INSERT INTO tabla_dias_disponibles_cesgados VALUES(tupla_auxiliar.instalacion_dias_disponibles, tupla_auxiliar.porcentaje_de_ocupacion);
 END LOOP;
 RETURN QUERY EXECUTE 'SELECT * FROM tabla_dias_disponibles_cesgados';
 DROP TABLE tabla_auxiliar_id_fecha;
