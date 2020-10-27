2c04f0f49dc3ee940fdec81099587ee41210801b
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-27 11:39:45 -0300

    me corto la pija, OLVIDE PONER VALUES AL INSERTAR LOS VALORES POR LA CRESTA

--- Entrega3/todos_dias_disponibles.sql
+++ Entrega3/todos_dias_disponibles.sql
@@ -152,12 +152,11 @@ id := tupla_dias_permiso_auxiliar.iid;
 SELECT INTO tupla_auxiliar * FROM tabla_dias_disponibles WHERE tabla_dias_disponibles.instalaciones_id=id;
 INSERT INTO tabla_dias_disponibles_cesgados VALUES(tupla_auxiliar.instalaciones_id,tupla_auxiliar.instalacion_dias_disponibles, tupla_auxiliar.porcentaje_de_ocupacion);
 END LOOP;
+SELECT INTO tupla_dias_string_to_array * FROM tabla_dias_disponibles_cesgados WHERE tabla_dias_disponibles_cesgados.instalaciones_id=instalacion_iid;
 string_to_array := (SELECT * FROM STRING_TO_ARRAY(tupla_dias_string_to_array.instalacion_dias_disponibles,','));
 FOR array_text IN SELECT * FROM string_to_array
 LOOP
-id := 1;
-SELECT INTO tupla_dias_string_to_array * FROM tabla_dias_disponibles_cesgados WHERE tabla_dias_disponibles_cesgados.instalaciones_id=instalacion_iid;
-INSERT INTO tabla_dias_disponibles_cesgados_instalacion_particular(id,array_text,'matame');
+INSERT INTO tabla_dias_disponibles_cesgados_instalacion_particular VALUES(tupla_dias_string_to_array.instalaciones_id,array_text,tupla_dias_string_to_array.porcentaje_de_ocupacion);
 END LOOP;
 RETURN QUERY SELECT * FROM tabla_dias_disponibles_cesgados_instalacion_particular;
 DROP TABLE tabla_auxiliar_id_fecha;
