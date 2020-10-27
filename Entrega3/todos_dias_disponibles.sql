12b3781f86fe87c3d63907eaf4563dab0cb9d9bf
Ignacio Medel <ingacio.medel@uc.cl>
2020-10-27 11:00:28 -0300

    go back to the previous definition of the array type

--- Entrega3/todos_dias_disponibles.sql
+++ Entrega3/todos_dias_disponibles.sql
@@ -153,7 +153,8 @@ SELECT INTO tupla_auxiliar * FROM tabla_dias_disponibles WHERE tabla_dias_dispon
 INSERT INTO tabla_dias_disponibles_cesgados VALUES(tupla_auxiliar.instalaciones_id,tupla_auxiliar.instalacion_dias_disponibles, tupla_auxiliar.porcentaje_de_ocupacion);
 END LOOP;
 SELECT INTO tupla_dias_string_to_array * FROM tabla_dias_disponibles_cesgados WHERE tabla_dias_disponibles_cesgados.instalaciones_id=instalacion_iid;
-FOREACH array_text IN STRING_TO_ARRAY(tupla_dias_string_to_array.instalacion_dias_disponibles,',')::TEXT[]
+string_to_array := STRING_TO_ARRAY(tupla_dias_string_to_array.instalacion_dias_disponibles,',')::TEXT[];
+FOREACH array_text IN string_to_array
 LOOP
 INSERT INTO tabla_dias_disponibles_cesgados_instalacion_particular(tupla_dias_string_to_array.instalaciones_id,array_text,tupla_dias_string_to_array.porcentaje_de_ocupacion);
 END LOOP;
