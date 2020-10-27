27ce26cd47fceec964072b22562da0a999cfaae7
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-27 11:25:58 -0300

    fixed creat

--- Entrega3/print_available.sql
+++ Entrega3/print_available.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION print_available(puerto, VARCHAR tipo_chosen VARCHAR, fecha_start DATE, fecha_end DATE)
+CREATE OR REPLACE FUNCTION print_available(puerto VARCHAR, tipo_chosen VARCHAR, fecha_start DATE, fecha_end DATE)
 RETURNS TABLE(iid INT, tipo VARCHAR(100), capacidad INT) 
 AS $$
 DECLARE
