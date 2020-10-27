364d2b9019a360ce7bb60aa36d7b72df0bfb08c0
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-26 21:54:31 -0300

    specified table names

--- Entrega3/calcular_capacidad.sql
+++ Entrega3/calcular_capacidad.sql
@@ -18,13 +18,13 @@ BEGIN
 CREATE TABLE table_cap(iid INT, tiene_capacidad bool);
 CREATE TABLE table_moors(pid INT, iid INT, capacidad INT, fecha_atraque DATE);
 FOR tupla_muelle IN SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT atraques.pid, instas.iid, instas.capacidad FROM
- (SELECT int_p.iid, tipo, capacidad FROM (SELECT iid FROM pertenece_a WHERE nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE tipo = 'muelle') AS instas
+ (SELECT int_p.iid, instalaciones.tipo, instalaciones.capacidad FROM (SELECT pertenece_a.iid FROM pertenece_a WHERE pertenece_a.nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE instalaciones.tipo = 'muelle') AS instas
  INNER JOIN atraques ON instas.iid = atraques.iid) AS insts INNER JOIN permisos ON insts.pid = permisos.pid)
 LOOP
 INSERT INTO table_moors VALUES(tupla_muelle.pid, tupla_muelle.iid, tupla_muelle.capacidad, tupla_muelle.fecha_atraque);
 END LOOP;
 atracados_s := 0;
-FOR tupla_inst IN SELECT int_p.iid, tipo, capacidad FROM (SELECT iid FROM pertenece_a WHERE nombre_puerto = nombre_puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE tipo = 'muelle'
+FOR tupla_inst IN SELECT int_p.iid, instalaciones.tipo, instalaciones.capacidad FROM (SELECT pertenece_a.iid FROM pertenece_a WHERE pertenece_a.nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE instalaciones.tipo = 'muelle'
 LOOP
 atracados_s := (SELECT COUNT(*) FROM table_moors WHERE table_moors.iid = tupla_inst.iid AND table_moors.fecha_atraque = fecha_start);
 capacidad_max := tupla_inst.capacidad;
@@ -34,7 +34,7 @@ DROP TABLE table_moors;
 dias_int := fecha_start - fecha_end;
 atracados := ARRAY[dias_int];
 FOR tupla_asti_2 IN SELECT permisos_astillero.pid, astis.iid, astis.capacidad, astis.fecha_atraque, permisos_astillero.fecha_salida FROM ((SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT pid, instas.iid, instas.capacidad FROM 
-	((SELECT int_p.iid, tipo, capacidad FROM (SELECT iid FROM pertenece_a WHERE nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE tipo = 'astillero')) AS instas
+	(SELECT int_p.iid, instalaciones.tipo, instalaciones.capacidad FROM (SELECT pertenece_a.iid FROM pertenece_a WHERE pertenece_a.nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE instalaciones.tipo = 'astillero') AS instas
  INNER JOIN atraques ON instas.iid = atraques.iid) AS insts INNER JOIN permisos ON insts.pid = permisos.pid)) as astis INNER JOIN permisos_astillero ON astis.pid = permisos_astillero.pid) WHERE astis.iid = tupla_inst_2.iid
 LOOP
 IF tupla_asti_2.fecha_atraque >= fecha_start AND tupla_asti_2.fecha_atraque <= fecha_end
