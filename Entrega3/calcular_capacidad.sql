9538cf29742f2268282bbb0d4366995966bcaf32
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-10-26 21:47:19 -0300

    editing the procedure

--- Entrega3/calcular_capacidad.sql
+++ Entrega3/calcular_capacidad.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION calcular_capacidad(fecha_start DATE, fecha_end DATE)
+CREATE OR REPLACE FUNCTION calcular_capacidad(puerto VARCHAR, fecha_start DATE, fecha_end DATE)
 RETURNS TABLE(iid INT, tiene_capacidad bool)
 AS $$
 DECLARE
@@ -17,25 +17,25 @@ ind INT;
 BEGIN
 CREATE TABLE table_cap(iid INT, tiene_capacidad bool);
 CREATE TABLE table_moors(pid INT, iid INT, capacidad INT, fecha_atraque DATE);
-FOR tupla_muelle IN SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT atraques.pid, instalaciones.iid, instalaciones.capacidad FROM instalaciones
- INNER JOIN atraques ON instalaciones.iid = atraques.iid WHERE instalaciones.tipo = 'muelle') AS insts INNER JOIN permisos ON insts.pid = permisos.pid)
+FOR tupla_muelle IN SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT atraques.pid, instas.iid, instas.capacidad FROM
+ (SELECT int_p.iid, tipo, capacidad FROM (SELECT iid FROM pertenece_a WHERE nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE tipo = 'muelle') AS instas
+ INNER JOIN atraques ON instas.iid = atraques.iid) AS insts INNER JOIN permisos ON insts.pid = permisos.pid)
 LOOP
 INSERT INTO table_moors VALUES(tupla_muelle.pid, tupla_muelle.iid, tupla_muelle.capacidad, tupla_muelle.fecha_atraque);
 END LOOP;
 atracados_s := 0;
-FOR tupla_inst IN SELECT * FROM instalaciones WHERE tipo = 'muelle'
+FOR tupla_inst IN SELECT int_p.iid, tipo, capacidad FROM (SELECT iid FROM pertenece_a WHERE nombre_puerto = nombre_puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE tipo = 'muelle'
 LOOP
-atracados_s := (SELECT COUNT(*) FROM table_moors WHERE table_moors.iid = tupla_inst.iid);
+atracados_s := (SELECT COUNT(*) FROM table_moors WHERE table_moors.iid = tupla_inst.iid AND table_moors.fecha_atraque = fecha_start);
 capacidad_max := tupla_inst.capacidad;
 INSERT INTO table_cap VALUES(tupla_inst.iid, atracados_s < capacidad_max);
 END LOOP;
 DROP TABLE table_moors;
 dias_int := fecha_start - fecha_end;
 atracados := ARRAY[dias_int];
-FOR tupla_inst_2 IN SELECT * FROM instalaciones WHERE tipo = 'astillero'
-LOOP
-FOR tupla_asti_2 IN SELECT permisos_astillero.pid, astis.iid, astis.capacidad, astis.fecha_atraque, permisos_astillero.fecha_salida FROM ((SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT pid, instalaciones.iid, instalaciones.capacidad FROM instalaciones
- INNER JOIN atraques ON instalaciones.iid = atraques.iid WHERE instalaciones.tipo = 'astillero') AS insts INNER JOIN permisos ON insts.pid = permisos.pid)) as astis INNER JOIN permisos_astillero ON astis.pid = permisos_astillero.pid) WHERE astis.iid = tupla_inst_2.iid
+FOR tupla_asti_2 IN SELECT permisos_astillero.pid, astis.iid, astis.capacidad, astis.fecha_atraque, permisos_astillero.fecha_salida FROM ((SELECT permisos.pid, insts.iid, insts.capacidad, permisos.fecha_atraque FROM ((SELECT pid, instas.iid, instas.capacidad FROM 
+	((SELECT int_p.iid, tipo, capacidad FROM (SELECT iid FROM pertenece_a WHERE nombre_puerto = puerto) AS int_p INNER JOIN instalaciones ON int_p.iid = instalaciones.iid WHERE tipo = 'astillero')) AS instas
+ INNER JOIN atraques ON instas.iid = atraques.iid) AS insts INNER JOIN permisos ON insts.pid = permisos.pid)) as astis INNER JOIN permisos_astillero ON astis.pid = permisos_astillero.pid) WHERE astis.iid = tupla_inst_2.iid
 LOOP
 IF tupla_asti_2.fecha_atraque >= fecha_start AND tupla_asti_2.fecha_atraque <= fecha_end
 THEN 
@@ -72,8 +72,7 @@ atracados[fecha_aux - fecha_start] =  atracados[fecha_aux - fecha_start] + 1;
 fecha_aux := fecha_aux + 1;
 END LOOP;
 END IF;
-END LOOP;
-capacidad_max := tupla_inst_2.capacidad;
+capacidad_max := tupla_asti_2.capacidad;
 ind := 0;
 has_cap := true;
 LOOP
@@ -84,7 +83,7 @@ has_cap := false;
 END IF;
 ind := ind + 1;
 END LOOP;
-INSERT INTO table_cap VALUES(tupla_inst_2.iid, has_cap);
+INSERT INTO table_cap VALUES(tupla_asti_2.iid, has_cap);
 END LOOP;
 RETURN QUERY EXECUTE 'SELECT * FROM table_cap ORDER BY iid';
 DROP TABLE table_cap;
