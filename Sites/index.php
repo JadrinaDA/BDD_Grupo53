b80d0393518ecf858d2183e797872f34c0958f18
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-12-06 14:59:54 -0300

    edited php errors

--- Sites/index.php
+++ Sites/index.php
@@ -4,13 +4,14 @@
         <h3>Ingrese los campos que desea</h3>
         <div class="api-requester">
             <form align="center" action="mapa_mensajes.php" method="get">
-                <input type="hidden" name="ID" value="<?php echo $id_usuario ?>">
+                <label for="ID">ID Usuario:</label><br>
+                <input id="ID" type="int" name="ID">
                 <label for="desired">Busqueda Simple:</label><br>
-                <input id="desired"> type="text" name="desired">
+                <input id="desired" type="text" name="desired">
                 <label for="start">Fecha inicio:</label><br>
-                <input id="start" type="text" name="start">
+                <input id="start" type="date" name="start">
                 <label for="end">Fecha fin:</label><br>
-                <input id="end" type="text" name="end">
+                <input id="end" type="date" name="end">
             <input type="submit" value="Buscar">
             </form>
         </div>
