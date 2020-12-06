b772e5bda6de54a3115e2b06c07e6259dccbd8d3
Jadrina De Andrade <jadrinadeandrade@uc.cl>
2020-12-06 15:23:44 -0300

    used urlencode

--- Sites/mapa_mensajes.php
+++ Sites/mapa_mensajes.php
@@ -34,7 +34,7 @@
     );
     
     $context  = stream_context_create( $options );
-    $result = file_get_contents( 'https://iic2413-grupo14-53-2020-2.herokuapp.com/text-search', false, $context );
+    $result = file_get_contents( urlencode('https://iic2413-grupo14-53-2020-2.herokuapp.com/text-search'), false, $context );
     $response = json_decode($result, true);
 
     ?>
