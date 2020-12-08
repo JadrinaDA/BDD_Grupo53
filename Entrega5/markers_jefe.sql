7ce7649e641c33453364644ec4381988663ff076
Ignacio Medel <ingacio.medel@uc.cl>
2020-12-08 17:05:05 -0300

    procedimiento almacenado jefes_markers 5.0

--- Entrega5/markers_jefe.sql
+++ Entrega5/markers_jefe.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION markers_jefe (rut VARCHAR)
+CREATE OR REPLACE FUNCTION markers_jefe (rut_jefe VARCHAR)
 RETURNS VARCHAR
 AS $$
 DECLARE
@@ -9,12 +9,12 @@ coordenadas_x FLOAT;
 coordenadas_y FLOAT;
 coordenadas_string VARCHAR;
 BEGIN
-es_jefe := (SELECT COUNT(jefes.iid) FROM jefes WHERE jefes.rut=rut);
+es_jefe := (SELECT COUNT(jefes.iid) FROM jefes WHERE jefes.rut=rut_jefe);
 IF es_jefe = 1 THEN
 jefe_de := (SELECT jefes.iid FROM jefes WHERE jefes.rut=rut);
-nombre_puerto := (SELECT pertenece_a.nombre_puerto FROM pertenece_a WHERE pertenece_a.iid=jefe_de);
-coordenadas_x := (SELECT puerto_coords.latitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto);
-coordenadas_y := (SELECT puerto_coords.longitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto);
+nombre_puerto_jefe := (SELECT pertenece_a.nombre_puerto FROM pertenece_a WHERE pertenece_a.iid=jefe_de);
+coordenadas_x := (SELECT puerto_coords.latitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto_jefe);
+coordenadas_y := (SELECT puerto_coords.longitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto_jefe);
 coordenadas_string := CONCAT(coordenadas_x,',',coordenadas_y);
 ELSE 
 coordenadas_string := 'None';
