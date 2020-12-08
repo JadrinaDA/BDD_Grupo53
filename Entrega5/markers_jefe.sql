e51fe785d4ce50494404207b12c6eb858e9283d9
Ignacio Medel <ingacio.medel@uc.cl>
2020-12-08 17:03:06 -0300

    procedimiento almacenado jefes_markers 4.0

--- Entrega5/markers_jefe.sql
+++ Entrega5/markers_jefe.sql
@@ -9,12 +9,12 @@ coordenadas_x FLOAT;
 coordenadas_y FLOAT;
 coordenadas_string VARCHAR;
 BEGIN
-es_jefe := SELECT COUNT(jefes.iid) FROM jefes WHERE jefes.rut=rut;
+es_jefe := (SELECT COUNT(jefes.iid) FROM jefes WHERE jefes.rut=rut);
 IF es_jefe = 1 THEN
-jefe_de := SELECT jefes.iid FROM jefes WHERE jefes.rut=rut;
-nombre_puerto := SELECT pertenece_a.nombre_puerto FROM pertenece_a WHERE pertenece_a.iid=jefe_de;
-coordenadas_x := SELECT puerto_coords.latitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto;
-coordenadas_y := SELECT puerto_coords.longitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto;
+jefe_de := (SELECT jefes.iid FROM jefes WHERE jefes.rut=rut);
+nombre_puerto := (SELECT pertenece_a.nombre_puerto FROM pertenece_a WHERE pertenece_a.iid=jefe_de);
+coordenadas_x := (SELECT puerto_coords.latitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto);
+coordenadas_y := (SELECT puerto_coords.longitud FROM puerto_coords WHERE puerto_coords.puerto=nombre_puerto);
 coordenadas_string := CONCAT(coordenadas_x,',',coordenadas_y);
 ELSE 
 coordenadas_string := 'None';
